195190892425e9d22ce2940c7cd04e383251aafe
Nikola Kokic <nikolazaraznestvari@gmail.com>
2020-09-22 23:07:10 +0200

    Updates

---
+++ precache-manifest.ea6505801417b1fd0eaee61b629e2fa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4afcdceb66dc6ea5bfc55da2027f9f",
+    "url": "/react-todo-ap/index.html"
+  },
+  {
+    "revision": "7eea641af99b176e803c",
+    "url": "/react-todo-ap/static/css/main.dffd7804.chunk.css"
+  },
+  {
+    "revision": "e12d558d18768466628d",
+    "url": "/react-todo-ap/static/js/2.1b2c2eac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-ap/static/js/2.1b2c2eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eea641af99b176e803c",
+    "url": "/react-todo-ap/static/js/main.f539f2fc.chunk.js"
+  },
+  {
+    "revision": "49f5eb8ccc4ebbc7c773",
+    "url": "/react-todo-ap/static/js/runtime-main.cde58e8c.js"
+  }
+]);
